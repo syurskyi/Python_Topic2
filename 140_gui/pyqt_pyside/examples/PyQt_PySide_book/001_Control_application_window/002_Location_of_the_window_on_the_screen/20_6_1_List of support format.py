b50e53739be493b0525e20b44b589ab51bf16452
Grey <sergejyurskyj@yahoo.com>
2020-06-11 20:41:40 -0700

    Committed PyQt_PySide update files

--- 140_gui/pyqt_pyside/examples/PyQt_PySide_book/001_Control_application_window/002_Location_of_the_window_on_the_screen/20_6_1_List of support format.py
+++ 140_gui/pyqt_pyside/examples/PyQt_PySide_book/001_Control_application_window/002_Location_of_the_window_on_the_screen/20_6_1_List of support format.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from PySide import QtGui
+from PySide2 import QtGui, QtWidgets
 
 for i in QtGui.QImageReader.supportedImageFormats():
     print(str(i).upper(), end=" ")
